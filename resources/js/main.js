d37fbd6eeba60815353476db7e1c9d8295dd8cce
Jeo <jeopatto@gmail.com>
2018-02-15 17:05:16 -0500

    Implemented Listjs + tweaks

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,57 +1,92 @@
+//  ---------------------------------------------------------------------
+//  Page Tab Function ---------------------------------------------------
+//  ---------------------------------------------------------------------
 function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+	// Hide all elements with class="tabcontent" by default */
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
+	// Remove the background color of all tablinks/buttons
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].style.backgroundColor = "";
+	}
 
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
+	// Show the specific tab content
+	document.getElementById(pageName).style.display = "block";
 
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+	// Add the specific color to the button used to open the tab content
+	elmnt.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
 
-var request_options = {
-    valueNames: ['id', 'user', 'fac', 'dep', 'nop', 'reqdate', 'deptime', 'dest', 'contno', 'driver', 'stat', 'edit']
+//  ---------------------------------------------------------------------
+//  Reset List Function -------------------------------------------------
+//  ---------------------------------------------------------------------
+function resetList(list) {
+	list.search();
+	list.filter();
+	list.update();
+	list.sort('reqdate', {
+		order: "asc"
+	});
+	list.sort('status', {
+		order: "desc"
+	});
+	$(".filter-all").prop('checked', true);
+	$('.filter').prop('checked', false);
+	$('.search').val('');
+	//console.log('Reset Successfully!');
 };
 
-var requestList = new List('reqList', request_options);
 
-// var status = document.getElementsByClassName('stat');
 
-// if (status.textContent == "approved"){
-//     status.classList.addClass("good");
-// }
+// ----- TEMPLATE CODE DO NOT TOUCH!!!!!!!!! -------
+/* function updateList(){
+  var values_gender = $("input[name=gender]:checked").val();
+	var values_address = $("input[name=address]:checked").val();
+	console.log(values_gender, values_address);
 
-function resetReqList() {
-    resetList(requestList);
+	userList.filter(function (item) {
+		var genderFilter = false;
+		var addressFilter = false;
+		
+		if(values_gender == "all")
+		{ 
+			genderFilter = true;
+		} else {
+			genderFilter = item.values().gender == values_gender;
+			
+		}
+		if(values_address == null)
+		{ 
+			addressFilter = true;
+		} else {
+			addressFilter = item.values().address.indexOf(values_address) >= 0;
+		}
+		return addressFilter && genderFilter
+	});
+	userList.update();
+	//console.log('Filtered: ' + values_gender);
 }
-
-function resetList(listObj) {
-    // requestList.search();
-    // requestList.filter();
-    // requestList.update();
-    // $(".filter-all").prop('checked', true);
-    // $('.filter').prop('checked', false);
-    // $('.search').val('');
-    //console.log('Reset Successfully!');
-    document.querySelector('.search').value = "";
-    // $('.search').val('');
-    listObj.search("");
-};
-
-function updateList() {
-    var values_status = querySelector("input[name=status]:checked").value;
-}
+                               
+$(function(){
+  //updateList();
+  $("input[name=gender]").change(updateList);
+	$('input[name=address]').change(updateList);
+	
+	userList.on('updated', function (list) {
+		if (list.matchingItems.length > 0) {
+			$('.no-result').hide()
+		} else {
+			$('.no-result').show()
+		}
+	});
+}); */
+// ----- TEMPLATE CODE DO NOT TOUCH!!!!!!!!! -------
