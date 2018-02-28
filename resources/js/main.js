22c9ce154a2fe81a110c2f5a69c5a28f5182a5ba
Jeo <jeopatto@gmail.com>
2018-02-28 11:03:49 -0500

    Dashboard v2.0.1 - Dynatable Fixes

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,37 +1,37 @@
-$(document).ready(function () {
+(function ($) {
+	//Tab Functionality
+	$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
 
-	// TAB FUNCTIONALITY
-	(function ($) {
-		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+	$('.tab ul.tabs li a').click(function (g) {
+		var tab = $(this).closest('.tab'),
+			index = $(this).closest('li').index();
 
-		$('.tab ul.tabs li a').click(function (g) {
-			var tab = $(this).closest('.tab'),
-				index = $(this).closest('li').index();
+		tab.find('ul.tabs > li').removeClass('current');
+		$(this).closest('li').addClass('current');
 
-			tab.find('ul.tabs > li').removeClass('current');
-			$(this).closest('li').addClass('current');
+		tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+		tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
 
-			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
-			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
-
-			g.preventDefault();
-		});
-	})(jQuery);
-
-
-	// DYNATABLE INITIALIZATION
-	$('.reqtable').dynatable({
-		dataset: {
-			perPageDefault: 10,
-			perPageOptions: [10, 20, 30]
-		},
-		inputs: {
-			queries: $('#search-status')
-		}
-
+		g.preventDefault();
 	});
 
-	// SELECTRIC INITIALIZATION
-	$('select').selectric();
+	//Create Dynatable
+	function makeTable(tableId, queryId) {
+		return $(tableId).dynatable({
+			dataset: {
+				perPageDefault: 10,
+				perPageOptions: [10, 20, 30]
+			},
+			inputs: {
+				queries: $(queryId)
+			}
+		});
+	}
 
-});
+	//Dynatable Initializations
+	makeTable('#requestTable', "#search-status");
+	makeTable('#driverTable', "#search-department");
+
+	//Selectric
+	$('select').selectric();
+})(jQuery);
